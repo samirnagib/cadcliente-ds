9e4ecfdcb4743c3d78b36808921d99168bbad7e4
samirnagib <samir.nagib@gmail.com>
2022-05-23 20:57:31 -0300

    Second commit

---
+++ backend/src/test/java/com/devsuperior/samirnagib/dscadcliente/DscadclienteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.samirnagib.dscadcliente;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DscadclienteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
