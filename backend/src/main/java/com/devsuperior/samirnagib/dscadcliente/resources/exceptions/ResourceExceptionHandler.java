a0047875d714a669b695e3c0ae0c7fb6cfdd85b7
samirnagib <samir.nagib@gmail.com>
2022-05-23 21:45:39 -0300

    Criacao da entidade, recursos, execeptions

---
+++ backend/src/main/java/com/devsuperior/samirnagib/dscadcliente/resources/exceptions/ResourceExceptionHandler.java
@@ -0,0 +1,47 @@
+package com.devsuperior.samirnagib.dscadcliente.resources.exceptions;
+
+import java.time.Instant;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import com.devsuperior.samirnagib.dscadcliente.services.exceptions.DatabaseException;
+import com.devsuperior.samirnagib.dscadcliente.services.exceptions.ResourceNotFoundException;
+
+@ControllerAdvice
+public class ResourceExceptionHandler {
+
+	@ExceptionHandler(ResourceNotFoundException.class)
+	public ResponseEntity<StandartError> entityNotFound(ResourceNotFoundException e, HttpServletRequest request) {
+		HttpStatus status = HttpStatus.NOT_FOUND;
+		StandartError err = new StandartError();
+		err.setTimestamp(Instant.now());
+		err.setStatus(status.value());
+		err.setError("Resource not found");
+		err.setMessage(e.getMessage());
+		err.setPath(request.getRequestURI());
+		return ResponseEntity.status(status).body(err);
+	}
+	
+
+	@ExceptionHandler(DatabaseException.class)
+	public ResponseEntity<StandartError> database(DatabaseException e, HttpServletRequest request) {
+		HttpStatus status = HttpStatus.BAD_REQUEST;
+		StandartError err = new StandartError();
+		err.setTimestamp(Instant.now());
+		err.setStatus(status.value());
+		err.setError("Database exception");
+		err.setMessage(e.getMessage());
+		err.setPath(request.getRequestURI());
+		return ResponseEntity.status(status).body(err);
+	}
+	
+	
+	
+	
+	
+}
---
+++ backend/src/main/java/com/devsuperior/samirnagib/dscadcliente/services/ClientService.java
@@ -0,0 +1,35 @@
+package com.devsuperior.samirnagib.dscadcliente.services;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+
+import com.devsuperior.samirnagib.dscadcliente.dto.ClientDTO;
+
+public class ClientService {
+
+	public Page<ClientDTO> findAllPaged(PageRequest pageRequest) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public ClientDTO findById(Long id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public ClientDTO insert(ClientDTO dto) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public ClientDTO update(Long id, ClientDTO dto) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public void delete(Long id) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
